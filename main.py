5adc80da4738301717890fff88e970ea1aa4c30f
ethan-green3 <ethangreen2000@yahoo.com>
2025-05-06 15:22:19 -0500

    Player collision with asteroid now exits game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -29,7 +30,10 @@ def main():
                 return
 
         updatable.update(dt)
-
+        for obj in asteroids:
+            if obj.check_collision(player):
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
 
         for obj in drawable:
